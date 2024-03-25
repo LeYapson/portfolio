c066f5a180cf9ff9575f9a3d9a9eafbae77d7509
théau Yapi <theau.yapi@ynov.com>
2024-03-25 14:15:51 +0100

    switch to fr

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -56,10 +56,10 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} sm:pl-16 pl-[2rem]`}>
-          What I've done so far
+        Ce que j'ai fait jusqu'à présent
         </p>
         <h2 className={`${styles.sectionHeadText} sm:pl-16 pl-[2rem]`}>
-          Work Experience.
+        Expérience professionnelle.
         </h2>
       </motion.div>
 
