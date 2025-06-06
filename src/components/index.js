93417afa5b81481b8597267c3bacfad54aa2365b
theau <theau.yapi@ynov.com>
2025-06-06 14:54:37 +0200

    modif portfolio

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,6 @@ import Tech from './Tech';
 import Experience from './Experience';
 import Projects from './Projects';
 import Contact from './Contact';
+import Loader from './Loader';
 
-export { Hero, Navbar, About, Tech, Experience, Projects, Contact, BallCanvas };
+export { Hero, Navbar, About, Tech, Experience, Projects, Contact, BallCanvas, Loader };
